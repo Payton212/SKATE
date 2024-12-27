55ff7ec559bfa74e4b863ce878f8f41ea8b92323
payton <payton@paytons-Air.lan>
2024-12-27 08:34:45 -0700

    added basic kickflip nad heelflip to the easy difficulty

--- assets/index.js
+++ assets/index.js
@@ -25,28 +25,40 @@ gt.addEventListener("click", function (event) {
   event.preventDefault();
   if (dEasy.checked === true) {
     let newTrick = easy();
-    if(newTrick.stance === 'regular' && newTrick.direction === 'frontside' && newTrick.rotation === '180' && newTrick.shuv === 'none'){
+    console.log(completedTricks);
+    if(newTrick.stance === 'regular' && newTrick.direction === 'frontside' && newTrick.rotation === '180' && newTrick.shuv === 'none' && newTrick.flip === 'none'){
          namedTrick = 'front 180';
-    }else if(newTrick.stance === 'regular' && newTrick.direction === 'frontside' && newTrick.rotation === 'none' && newTrick.shuv === '180'){
+    }else if(newTrick.stance === 'regular' && newTrick.direction === 'frontside' && newTrick.rotation === 'none' && newTrick.shuv === '180' && newTrick.flip === 'none'){
         namedTrick = 'front shuv';    
-    }else if(newTrick.stance === 'regular' && newTrick.direction === 'backside' && newTrick.rotation === '180' && newTrick.shuv === 'none'){
+    }else if(newTrick.stance === 'regular' && newTrick.direction === 'backside' && newTrick.rotation === '180' && newTrick.shuv === 'none' && newTrick.flip === 'none'){
         namedTrick = 'back 180';
-    }else if(newTrick.stance === 'regular' && newTrick.direction === 'backside' && newTrick.rotation === 'none' && newTrick.shuv === '180'){
+    }else if(newTrick.stance === 'regular' && newTrick.direction === 'backside' && newTrick.rotation === 'none' && newTrick.shuv === '180' && newTrick.flip === 'none'){
         namedTrick = 'back shuv';
-    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'frontside' && newTrick.rotation === '180' && newTrick.shuv === 'none'){
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'frontside' && newTrick.rotation === '180' && newTrick.shuv === 'none' && newTrick.flip === 'none'){
         namedTrick = 'frontside half cab';
-    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'frontside' && newTrick.rotation === 'none' && newTrick.shuv === '180'){
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'frontside' && newTrick.rotation === 'none' && newTrick.shuv === '180' && newTrick.flip === 'none'){
         namedTrick = 'fakie front shuv';
-    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'backside' && newTrick.rotation === '180' && newTrick.shuv === 'none'){
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'backside' && newTrick.rotation === '180' && newTrick.shuv === 'none' && newTrick.flip === 'none'){
         namedTrick = 'half cab';
-    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'backside' && newTrick.rotation === 'none' && newTrick.shuv === '180'){
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'backside' && newTrick.rotation === 'none' && newTrick.shuv === '180' && newTrick.flip === 'none'){
         namedTrick = 'fakie back shuv';
-    }
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'none' && newTrick.rotation === 'none' && newTrick.shuv === 'none' && newTrick.flip === 'kickflip'){
+        namedTrick = 'fakie kickflip';
+    }else if(newTrick.stance === 'fakie' && newTrick.direction === 'none' && newTrick.rotation === 'none' && newTrick.shuv === 'none' && newTrick.flip === 'heelflip'){
+        namedTrick = 'fakie heelflip';
+    }else if(newTrick.stance === 'regular' && newTrick.direction === 'none' && newTrick.rotation === 'none' && newTrick.shuv === 'none' && newTrick.flip === 'kickflip'){
+        namedTrick = 'kickflip';
+    }else if(newTrick.stance === 'regular' && newTrick.direction === 'none' && newTrick.rotation === 'none' && newTrick.shuv === 'none' && newTrick.flip === 'heelflip'){
+        namedTrick = 'heelflip';
+    } 
+
+    
     nt.textContent = namedTrick;
     pst.textContent = newTrick.stance;
     pd.textContent = newTrick.direction;
     pr.textContent = newTrick.rotation;
     ps.textContent = newTrick.shuv;
+    pKH.textContent = newTrick.flip
 
 
     
@@ -61,23 +73,29 @@ function easy() {
   let stances = ["regular", "fakie"];
   let directions = ["frontside", "backside"];
   let rotations = ["180", "none"];
-
-  if(completedTricks.length === 8){
+  let flips = ['kickflip', 'heelflip', 'none'];
+  let shuvs = ['180','none'];
+  if(completedTricks.length === 12){
     return `all tricks generated`
   }else 
   do {
     randomStance = gRandomStance(stances);
     randomDirection = gRandomDirection(directions);
     randomRotation = gRandomRotations(rotations);
-    
+    randomFlip = gRandomFlip(flips);
+    randomShuvs = gRandomShuvs(shuvs)
     // Determine shuv based on rotation
-    if (randomRotation === "180") {
-      randomShuvs = "none";
-    } else {
-      randomShuvs = "180";
+    if (randomFlip !== 'none') {
+      randomShuvs = 'none'
+      randomRotation = 'none'
+      randomDirection = 'none';
+    } else if (randomFlip === 'none' && randomRotation === 'none'){
+        randomShuvs = '180';
+    }else if(randomFlip === 'none' && randomRotation === '180'){
+        randomShuvs = 'none'
     }
-
-  } while (completedTricks.some(trick => 
+   }while (completedTricks.some(trick => 
+    trick.flip === randomFlip &&
     trick.stance === randomStance && 
     trick.direction === randomDirection &&  
     trick.rotation === randomRotation &&
@@ -89,12 +107,14 @@ function easy() {
       direction: randomDirection,
       rotation: randomRotation,
       shuv: randomShuvs,
+      flip: randomFlip,
     });
     return {
         stance: randomStance,
         direction: randomDirection,
         rotation: randomRotation,
         shuv: randomShuvs,
+        flip: randomFlip,
       };
 }
 
@@ -113,5 +133,8 @@ function gRandomRotations(rotations) {
 }
 function gRandomShuvs(shuvs) {
   return shuvs[Math.floor(Math.random() * shuvs.length)];
+}
+function gRandomFlip(flips){
+  return flips[Math.floor(Math.random() * flips.length)];
 }
 
